fbb9ded75ed494b47efc97fce153f569420b8397
xieziwei99 <xieziwei99@gmail.com>
2020-05-29 13:43:25 +0800

    Hbase helloworld2

--- hbase/src/alohahbase/AlohaHbase.java
+++ hbase/src/alohahbase/AlohaHbase.java
@@ -30,8 +30,16 @@ public class AlohaHbase {
 
     public static void main(String[] args) throws IOException {
         createTable("student", "score");
+        insertData("student", "zhangsan", "score", "English", "69");
+        insertData("student", "zhangsan", "score", "Math", "86");
+        insertData("student", "zhangsan", "score", "Computer", "100");
+        String computerScore = new String(getData("student", "zhangsan", "score", "Computer"));
+        System.out.println("张三的计算机分数为 " + computerScore);
     }
 
+    /**
+     * Hbase 初始化 创建 configuration connection admin
+     */
     public static void init() {
         configuration = HBaseConfiguration.create();
         configuration.set("hbase.rootdir", "hdfs://localhost:9000/hbase");
@@ -44,6 +52,9 @@ public class AlohaHbase {
         }
     }
 
+    /**
+     * Hbase 关闭 connection admin
+     */
     public static void close() {
         try {
             if (admin != null) {
@@ -57,6 +68,12 @@ public class AlohaHbase {
         }
     }
 
+    /**
+     * 创建 Hbase 表
+     *
+     * @param tableName   表名
+     * @param colFamilies 列族名，可变参数
+     */
     public static void createTable(String tableName, String... colFamilies) throws IOException {
         init();
         TableName tableNameObject = TableName.valueOf(tableName);
@@ -74,5 +91,50 @@ public class AlohaHbase {
         admin.createTable(tableDescriptor);
         close();
         System.out.println(tableName + "表添加成功");
+    }
+
+    /**
+     * 插入一条数据
+     *
+     * @param tableName 表名
+     * @param rowKey    行健
+     * @param colFamily 列族
+     * @param col       列
+     * @param value     值
+     * @throws IOException 如果表不存在，会抛出如 org.apache.hadoop.hbase.TableNotFoundException: student
+     *                     其他原因如网络等也会抛出异常
+     */
+    public static void insertData(String tableName, String rowKey, String colFamily, String col, String value) throws IOException {
+        init();
+        Table table = connection.getTable(TableName.valueOf(tableName));    // 表不存在会抛出异常
+        Put put = new Put(Bytes.toBytes(rowKey));
+        put.addColumn(Bytes.toBytes(colFamily), Bytes.toBytes(col), Bytes.toBytes(value));
+        table.put(put);
+        table.close();
+        close();
+        System.out.println("插入数据成功");
+    }
+
+    /**
+     * 获取数据
+     *
+     * @param tableName 表名
+     * @param rowKey    行健
+     * @param colFamily 列族 如果为空，会出现空指针异常
+     * @param col       列 如果为空，会出现空指针异常
+     * @return 该单元格的值
+     * @throws IOException 如果表不存在，会抛出如 org.apache.hadoop.hbase.TableNotFoundException: student
+     *                     其他原因如网络等也会抛出异常
+     */
+    public static byte[] getData(String tableName, String rowKey, String colFamily, String col) throws IOException {
+        init();
+        Table table = connection.getTable(TableName.valueOf(tableName));    // 表不存在会抛出异常
+        Get get = new Get(Bytes.toBytes(rowKey));
+        get.addColumn(Bytes.toBytes(colFamily), Bytes.toBytes(col));
+        Result result = table.get(get);
+        byte[] value = result.getValue(Bytes.toBytes(colFamily), Bytes.toBytes(col));
+        table.close();
+        close();
+        return value;
     }
 }
